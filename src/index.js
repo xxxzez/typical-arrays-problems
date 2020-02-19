1832f2ff8b7853de9086db3452fbae6c2548588c
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-19 19:30:30 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
 exports.min = function min (array) {
-  return 0;
+    if ( arguments.length == 0 || array.length == 0) return 0;
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( arguments.length == 0 || array.length == 0) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( arguments.length == 0 || array.length == 0) return 0;
+    return array.reduce((a, b) => (a + b)) / array.length;
 }
